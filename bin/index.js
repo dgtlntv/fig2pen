39d1688758da7e3ded358599ec7ea32554309b70
Maximilian Blazek <68336485+dgtlntv@users.noreply.github.com>
2023-10-16 14:34:55 +0200

    pseudo code of the rough process

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,53 @@
 #!/usr/bin/env node
 
-console.log("hello world :)")
+/*
+function migrateFigmaToPenpot():
+
+    // Get all Figma projects
+    projects = FigmaAPI.getAllProjects()
+
+    // Create a data structure to save the project structure
+    projectData = []
+
+    for project in projects:
+        
+        // For each project, get the project's files
+        projectFiles = FigmaAPI.getFilesForProject(project)
+
+        // Save the project structure
+        projectData.append({
+            'project': project,
+            'files': projectFiles
+        })
+
+        // Look at version history to extract editors
+        editors = []
+        for file in projectFiles:
+            versionHistory = FigmaAPI.getVersionHistory(file)
+            fileEditors = extractEditorsFromVersionHistory(versionHistory) // A function to parse version history and get editors
+            editors.extend(fileEditors)
+
+        // Save all editors in a list (removing duplicates)
+        uniqueEditors = removeDuplicates(editors) // A function to remove duplicate entries
+
+    // Create a team in Penpot
+    team = PenpotAPI.createTeam()
+
+    // Invite all the unique editors to the team
+    PenpotAPI.inviteEditorsToTeam(uniqueEditors, team)
+
+    // Recreate the project structure in Penpot
+    for data in projectData:
+        newProject = PenpotAPI.createProject(data['project'])
+        
+        for file in data['files']:
+            fileJson = FigmaAPI.getFileJson(file)
+
+            // Convert Figma JSON to Penpot file
+            penpotFile = convertFigmaJsonToPenpotFile(fileJson) // A function to handle the conversion
+
+            // Import Penpot files to their respective projects
+            PenpotAPI.importFileToProject(penpotFile, newProject)
+
+    return "Migration completed!"
+*/
